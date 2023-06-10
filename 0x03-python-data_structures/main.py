795a42da233f57d7c30d7342b97addc1edd8964a
FaresRez <fers.rez16@gmail.com>
2023-06-10 09:23:35 +0000

    Fix return none

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+element_at = __import__('1-element_at').element_at
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
+my_list = [1, 2, 3, 4, 5]
+idx = 0
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
