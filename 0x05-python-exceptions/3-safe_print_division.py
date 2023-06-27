bb7b43a932da42734c439ca4096088b1a18dcb33
FaresRez <fers.rez16@gmail.com>
2023-06-27 02:18:58 +0000

    Fix print stat

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    n = 0
     try:
         n = a / b
     except ZeroDivisionError:
         return None
     finally:
-        print("{:d}".format(n))
-        return n
+        print("Inside result: {:.1f}".format(n))
+    return n
