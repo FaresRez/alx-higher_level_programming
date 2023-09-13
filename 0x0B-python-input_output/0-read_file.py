a33e39cc987cdfa01e41f7546b5ee952203f4672
FaresRez <fers.rez16@gmail.com>
2023-09-13 16:04:19 +0000

    Fix documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+    """
+        the program read files
+    """
 def read_file(filename=""):
     """
         read a given file
     """
     with open(filename, 'r') as f:
-         f.read()
+         print(f.read())
          f.close()
