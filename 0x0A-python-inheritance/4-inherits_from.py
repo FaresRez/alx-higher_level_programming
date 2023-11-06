53531699b718e241545248cc87640e2004c0ec9b
Feres89 <fers.rez16@gmail.com>
2023-11-06 22:15:54 +0100

    Create a class BaseGeometry

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-"""return true if the object is an instance of the specified class"""
+"""returns True if the object is an instance of a class that inherited"""
 
 
 def inherits_from(obj, a_class):
-    """Verifiying is the instance beyong to a specific class or not"""
-    return isinstance(obj, a_class)
-
-
-a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object.__name__))
+    """return true or false"""
+    return isinstance(obj, a_class) and type(obj) != a_class
