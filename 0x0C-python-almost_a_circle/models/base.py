66f63794d3d0a7e3fc73e602a2f3a9f47b7f11cc
FaresRez <fers.rez16@gmail.com>
2023-11-16 17:07:53 +0100

    Add save to file method in Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 """The class Base that handle id"""
 
 
@@ -11,3 +12,18 @@ class Base:
            self.id = Base.__nb_objects
         else:
             self.id = id
+
+    def to_json_string(list_dictionaries):
+        """Return a JSON from a list of dict"""
+        return json.dumps([x for x in list_dictionaries])
+    
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save the JSON obj into file"""
+        with open(f'{cls.__name__}.json',mode="w", encoding="utf-8")as f:
+            f.write([obj.to for obj in list_objs])
+
+
+
+
+
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -119,3 +119,6 @@ class Rectangle(Base):
 
 
 
+r1 = Rectangle(10, 7, 2, 8)
+r2 = Rectangle(2, 4)
+Rectangle.save_to_file([r1, r2])
