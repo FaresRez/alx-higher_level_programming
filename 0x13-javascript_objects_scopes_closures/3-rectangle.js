a6cb2e328a7643e637535b31c1ec8d15e035f3f8
Feres89 <fers.rez16@gmail.com>
2024-01-09 22:04:56 +0100

    Add header

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,25 +1,18 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
-    for (let i = 0; i < this.height; i++) { 
-      for (let j = 0; j < this.width; j++) 
-        process.stdout.write("X");
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) process.stdout.write('X');
       console.log();
     }
   }
 }
 
 module.exports = Rectangle;
-
-const r1 = new Rectangle(2, 3);
-r1.print();
-
-const r2 = new Rectangle(10, 5);
-r2.print();
