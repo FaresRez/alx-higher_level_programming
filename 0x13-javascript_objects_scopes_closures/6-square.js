079e94d11a1ed4951284bc41d44d0c8b49ea09d4
Feres89 <fers.rez16@gmail.com>
2024-01-09 22:36:56 +0100

    Create a square class

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const Square_ = require("./5-square");
+
+const Square = class Square extends Square_ {
+  constructor(size) {
+    super(size);
+  }
+
+  charPrint(c) {
+    if (c === undefined)
+      super.print();
+    else
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++)
+          process.stdout.write(c);
+        console.log();
+      }
+  }
+};
+
+module.exports = Square;
+
