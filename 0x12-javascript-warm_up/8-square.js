79834aeaba2ae0b6473f6fbbbffebfa23af5c99c
FaresRez <fers.rez16@gmail.com>
2024-01-10 15:51:15 +0100

    Fix the square output

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@ if (isNaN(Number(process.argv[2])) || process.argv[2] === undefined) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < process.argv[2]; i++) {
-    console.log('x');
-  }
-  for (let i = 0; i < process.argv[2]; i++) {
-    console.log('x');
+    for (let i = 0; i < process.argv[2]; i++) {
+      process.stdout.write('X');
+    }
+    console.log();
   }
 }
