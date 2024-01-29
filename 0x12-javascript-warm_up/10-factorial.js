5d1c9280c6f3faa81326e92ebf0a8df7a48b23ad
Fares <69602373+FaresRez@users.noreply.github.com>
2024-01-29 19:43:40 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/10-factorial.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-function factorial (a, b) {
-    if 
-    return factorial(a, b)*
-  }
-  
-  factorial(Number(process.argv[2]), Number(process.argv[3]));
--- 0x12-javascript-warm_up/7-multi_c.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-if (isNaN(Number(process.argv[2])) || process.argv[2] === undefined) {
-  console.log('Missing number of occurrences');
-} else {
-  for (let i = 0; i < process.argv[2]; i++) {
-    console.log('C is fun');
-  }
-}
