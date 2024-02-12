81d7d968c4e73401bbb19fa7aa79000435486e8d
FaresRez <fers.rez16@gmail.com>
2024-02-12 10:44:04 +0100

    Fix semistandard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 if (isNaN(Number(process.argv[2])) || process.argv[2] === undefined) {
-    console.log('Missing size');
-  } else {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < process.argv[2]; i++) {
     for (let i = 0; i < process.argv[2]; i++) {
-      for (let i = 0; i < process.argv[2]; i++) {
-        process.stdout.write('X');
-      }
-      console.log();
+      process.stdout.write('X');
     }
+    console.log();
   }
+}
