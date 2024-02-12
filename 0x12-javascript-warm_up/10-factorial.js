524d880089b9ef4a834ac0bd534d30960988e28c
Feres89 <fers.rez16@gmail.com>
2024-02-12 22:14:36 +0100

    Fix factoriel file

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-function factorial (a, b) {
-    if (b == NaN || a == 0 || a == NaN)
-    return 1;
-else
-    return a*factorial(a-1, b);
+function factorial (a) {
+  if (a < 0) { return -1; }
+  if (a === 0 || isNaN(a)) { return 1; } else { return a * factorial(a - 1); }
 }
-  
-  console.log(factorial(Number(process.argv[2]), Number(process.argv[3])));
+
+console.log(factorial(Number(process.argv[2])));
