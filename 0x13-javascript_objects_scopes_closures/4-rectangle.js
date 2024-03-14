d22b8d3451b58996eb90b680d70cf066091d8efa
FaresRez <fers.rez16@gmail.com>
2024-03-14 08:42:41 +0100

    Create a square class extended from Rectangle

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -9,9 +9,20 @@ class Rectangle {
 
   print () {
     for (let i = 1; i <= this.height; i++) {
-      console.log();
       for (let j = 1; j <= this.width; j++) { process.stdout.write('X'); }
+      console.log();
     }
+  }
+
+  rotate () {
+    const ex = this.width;
+    this.width = this.height;
+    this.height = ex;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
   }
 }
 module.exports = Rectangle;
