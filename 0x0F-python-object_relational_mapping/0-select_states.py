2ff3121f38d45d9c801fa00ed5764d6a2ab6381c
FaresRez <fers.rez16@gmail.com>
2024-06-24 13:44:06 -0700

    Fix code not exe when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,18 +3,19 @@
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        password=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
+if __name__ = "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            password=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+        )
 
-cursor = db.cursor()
+    cursor = db.cursor()
 
-query = "SELECT * FROM states"
-cursor.execute(query)
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+    query = "SELECT * FROM states"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
