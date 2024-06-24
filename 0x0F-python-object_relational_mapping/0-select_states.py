db96ad5141f426374bb9b477f778e06a2ceb728b
FaresRez <fers.rez16@gmail.com>
2024-06-24 13:39:56 -0700

    0. Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" lists id and name from a database"""
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+cursor = db.cursor()
+
+query = "SELECT * FROM states"
+cursor.execute(query)
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
