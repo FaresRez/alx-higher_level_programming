c6ce06a69b5843f7185e5d33482f2302b6dcd40d
Feres89 <fers.rez16@gmail.com>
2025-04-27 20:30:35 +0100

    fixing pycodestyle

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,8 +11,3 @@ class Square:
         if (size < 0):
             raise ValueError("size must be >= 0")
         self.__size = size
-
-
-my_square_3 = Square(-3)
-print(type(my_square_3))
-print(my_square_3.__dict__)
