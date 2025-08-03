54c82e2c77206069b6c4f0cd01829fa8d2b0ea32
Feres89 <fers.rez16@gmail.com>
2025-08-03 16:32:26 +0100

    add a file that add the file parameter into a list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -14,10 +14,7 @@ if __name__ == "__main__":
     load_from_json_file = importlib.import_module('6-load_from_json_file')\
         .load_from_json_file
 
-    try:
-        my_list = list(load_from_json_file("add_item.json"))
-    except (FileNotFoundError, json.JSONDecodeError):
-        my_list = []
+    my_list = list(load_from_json_file("add_item.json"))
 
     my_list.extend(sys.argv[1:])
     save_to_json_file(my_list, "add_item.json")
