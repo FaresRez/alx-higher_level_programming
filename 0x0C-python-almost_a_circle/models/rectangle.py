7ed63d4f7174886a8c10a1a5751d351abe7f8b99
Feres89 <fers.rez16@gmail.com>
2025-08-17 19:05:38 +0100

    correct comments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,6 @@ class Rectangle(Base):
     """Rectangle class that inherits from Base."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize Rectangle instances."""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -24,44 +23,36 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Get the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set the width of the rectangle."""
         self.setters_validation(value, "width")
         self.__width = value
 
     @property
     def height(self):
-        """Get the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set the height of the rectangle."""
         self.setters_validation(value, "height")
         self.__height = value
 
     @property
     def x(self):
-        """Get the x coordinate of the rectangle."""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Set the x coordinate of the rectangle."""
         self.setters_validation(value, "x")
         self.__x = value
 
     @property
     def y(self):
-        """Get the y coordinate of the rectangle."""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Set the y coordinate of the rectangle."""
         self.setters_validation(value, "y")
         self.__y = value
