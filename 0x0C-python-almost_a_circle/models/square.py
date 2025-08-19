9560900acfeb9aca46e9f0b081d70358aeb8a265
Feres89 <fers.rez16@gmail.com>
2025-08-19 22:46:06 +0100

    Add square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,17 +4,19 @@ from rectangle import Rectangle
 
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """Square class inherit from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """Return a string representation of the square."""
         return f'[Square] ({self.id}) {self.x}/{self.y} - {self.size}'
 
     @property
     def size(self):
-        """"""
+        """Return the size of the square."""
         return self.width
 
     @size.setter
