3f0d42d88c303930cf1dcf9c81fba8b9cf6e69bf
Feres89 <fers.rez16@gmail.com>
2025-08-19 22:43:34 +0100

    Add square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,15 +8,14 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
-
     def __str__(self):
         return f'[Square] ({self.id}) {self.x}/{self.y} - {self.size}'
-    
+
     @property
     def size(self):
         """"""
         return self.width
-    
+
     @size.setter
     def size(self, value):
         self.width = value
@@ -24,7 +23,7 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """Update rectangle's attributes (if possible).
-        
+
             Args:
             *args (ints): New attribute values.
                 1st argument represents id attribute
@@ -41,25 +40,10 @@ class Square(Rectangle):
             attribut_list = ['id', 'size', 'x', 'y']
             for attribut, arg in zip(attribut_list, args):
                 setattr(self, attribut, arg)
-    
+
     def to_dictionary(self):
-         """Return a dictionary with all the object's attributes."""
-         return {"id": getattr(self, "id"),
+        """Return a dictionary with all the object's attributes."""
+        return {"id": getattr(self, "id"),
                 "size": getattr(self, "size"),
                 "x": getattr(self, "x"),
                 "y": getattr(self, "y")}
-
-
-
-
-s1 = Square(10, 2, 1)
-print(s1)
-s1_dictionary = s1.to_dictionary()
-print(s1_dictionary)
-print(type(s1_dictionary))
-
-s2 = Square(1, 1)
-print(s2)
-s2.update(**s1_dictionary)
-print(s2)
-print(s1 == s2)
