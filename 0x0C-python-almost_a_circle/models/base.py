18d18157776d0e99dcfa47a99d9affa440b478a0
Feres89 <fers.rez16@gmail.com>
2025-08-20 10:46:24 +0100

    Add static method dict to json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """The class Base that handle id"""
 
+import json
 
 class Base:
     """Base class
@@ -15,3 +16,9 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
