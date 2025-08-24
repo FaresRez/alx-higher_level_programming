ca1f73df0c8654667cd53431bb906c9e6c32ea12
Feres89 <fers.rez16@gmail.com>
2025-08-24 22:30:53 +0100

    Add create class  methods

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -39,4 +39,24 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
+        """Return the list of the JSON string representation json_string."""
         return json.loads(json_string) if json_string else []
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return an instance with all attributes already set."""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of instances."""
+        filename = cls.__name__ + ".json"
+        with open(filename, 'r') as f:
+            f.read(dict)
+            list_inst = cls.from_json_string(dict)
+        return [cls.create(**inst) for inst in list_inst]
